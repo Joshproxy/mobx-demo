cdd0b3568838f286b0bf96f26077261309bc1e43
Josh Naro <joshnaro@gmail.com>
2019-05-03 11:43:30 -0500

    Added a wrapper for the field class.

--- src/components/title/EditableTitle.tsx
+++ src/components/title/EditableTitle.tsx
@@ -24,20 +24,35 @@ const email: (value: string) => ValidatorResponse = (val: string) =>
   val.indexOf("@") === -1 && "not an email";
 
 // tslint:disable-next-line: max-classes-per-file
+/** Wraps the FieldState class to map from input element value to field value. */
+class FieldStateWrapper<T> extends FieldState<T> {
+  constructor(initialValue: T) {
+    super(initialValue);
+  }
+
+  /** Maps the target input element value to the field value with optional map function. */
+  public onInputChange = <I extends HTMLInputElement = HTMLInputElement>(
+    map: (value: string) => any = s => s
+  ) => (ev: React.ChangeEvent<I>) => {
+    this.onChange(map(ev.target.value));
+  };
+}
+
 /** This FormState wrapper exposes a couple extra methods for easy-of-use. */
+// tslint:disable-next-line: max-classes-per-file
 class FormStateWrapper<T> extends FormState<{
-  [key: string]: FieldState<any>;
+  [key: string]: FieldStateWrapper<any>;
 }> {
   /** Retrieves the value from the element and maps it into a field. */
   public static onFieldChange = (
-    fieldState: FieldState<any>,
+    fieldState: FieldStateWrapper<any>,
     map: (value: string) => any = s => s
   ) => (ev: React.ChangeEvent<HTMLInputElement>) => {
     fieldState.onChange(map(ev.target.value));
   };
   private initialValue: T;
 
-  constructor(init: T, fields: { [key: string]: FieldState<any> }) {
+  constructor(init: T, fields: { [key: string]: FieldStateWrapper<any> }) {
     super(fields);
     this.initialValue = init;
   }
@@ -67,8 +82,13 @@ export default class EditableTitle extends React.Component<ITitleProps> {
     super(props);
 
     this.titleForm = new FormStateWrapper<ITitle>(this.props.title, {
-      name: new FieldState(this.props.title.name).validators(required, email),
-      price: new FieldState<number>(this.props.title.price).validators(required)
+      name: new FieldStateWrapper(this.props.title.name).validators(
+        required,
+        email
+      ),
+      price: new FieldStateWrapper<number>(this.props.title.price).validators(
+        required
+      )
     });
   }
 
@@ -87,27 +107,24 @@ export default class EditableTitle extends React.Component<ITitleProps> {
           <Input
             id="nameChange"
             name="name"
-            type=""
-            onChange={FormStateWrapper.onFieldChange(form.$.name)}
+            onChange={form.$.name.onInputChange()}
             value={form.$.name.value}
           />
-          <p className="error">{form.$.name.error}</p>
+          <p className="validationError">{form.$.name.error}</p>
           <p>{title.description}</p>
           <Input
             id="priceChange"
             name="price"
             type="number"
-            onChange={FormStateWrapper.onFieldChange(form.$.price, s =>
-              parseInt(s, 10)
-            )}
+            onChange={form.$.price.onInputChange(s => parseInt(s, 10))}
             value={form.$.price.value}
           />
-          <p>{form.$.price.error}</p>
+          <p className="validationError">{form.$.price.error}</p>
           <p>Format: {title.format}</p>
           <button onClick={this.editTitleClick} className="addButton">
             Edit
           </button>
-          <p>{form.error}</p>
+          <p className="validationError">The form has an error: {form.error}</p>
         </div>
       </aside>
     );
